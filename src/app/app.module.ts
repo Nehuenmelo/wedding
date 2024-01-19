8f1a51c90e439ca7105d6267ac9135fd41437cfb
Nehuen Melo <nehuenmelo@gmail.com>
2024-01-19 04:29:40 -0300

    banner, ceremony, upload-file, we

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { FileUploadModule } from 'primeng/fileupload';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CountdownComponent } from './components/countdown/countdown.component';
+import { CeremonyComponent } from './components/ceremony/ceremony.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { WeComponent } from './components/we/we.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HomeComponent,
+    CountdownComponent,
+    CeremonyComponent,
+    UploadFileComponent,
+    WeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
